5e677d3eec4d9188b156502eb38bfdd21e3336d4
Pawel Szymczykowski <makenai@gmail.com>
2012-10-12 21:45:16 -0700

    Reconfigure for new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,18 @@
 set :application, "TreeDee"
-set :repository,  "set your repository location here"
+set :repository,  "git@github.com:makenai/TreeDee.git"
+set :user, "ec2-user"
+set :use_sudo, false
+
+ssh_options[:keys] = "~/.ssh/makenai.pem"
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "ec2-204-236-180-46.us-west-1.compute.amazonaws.com"                   # Your HTTP server, Apache/etc
+role :app, "ec2-204-236-180-46.us-west-1.compute.amazonaws.com"                   # This may be the same as your `Web` server
+role :db,  "ec2-204-236-180-46.us-west-1.compute.amazonaws.com", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +21,10 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
