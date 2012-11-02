d991995c9b74e029e0aa13e120ba578f20b277b2
Pawel Szymczykowski <makenai@gmail.com>
2012-11-02 01:49:13 -0700

    Compile assets locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,4 +30,13 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+  namespace :assets do
+    desc 'Run the precompile task locally and rsync with shared'
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      %x{bundle exec rake assets:precompile}
+      %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+      %x{bundle exec rake assets:clean}
+    end
+  end
+
 end
