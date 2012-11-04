c4b67f4f41252dc2f6c95e31997a223ce9b7f9f2
Pawel Szymczykowski <makenai@gmail.com>
2012-11-04 09:58:44 -0800

    One more time. no auto increment

--- db/migrate/20121009220356_create_object_data.rb
+++ db/migrate/20121009220356_create_object_data.rb
@@ -1,7 +1,7 @@
 class CreateObjectData < ActiveRecord::Migration
   def change
     create_table( :object_data, id: false ) do |t|
-      t.string :uuid, limit: 36, null: false, options: 'PRIMARY KEY'
+      t.column :uuid, 'CHAR(36) NOT NULL PRIMARY KEY'
       t.string :url
       t.timestamps
     end
